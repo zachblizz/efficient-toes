ca0791ff9c86122efc5088a283844642ee19044b
Zach.Blizzard <zach.blizzard@solera.com>
2020-03-04 13:47:51 -0600

    finish brain

--- src/Brain.js
+++ src/Brain.js
@@ -1,26 +1,22 @@
 const Diags = new Set(["00", "20", "02", "22", "11"]);
 export const EMPTY = "";
-
-function checkDiagRowAndCol(row, char, winner, board) {
+function checkDiagRowAndCol(row, col, char, winner, board) {
   let rowCount = 0;
   let colCount = 0;
-
   for (let i = 0; i < board.length; i++) {
-    if (board[i][row] !== EMPTY && board[i][row] !== char) {
+    if (board[i][col] !== EMPTY && board[i][col] !== char) {
       winner.winner = false;
-    } else if (board[i][row] !== EMPTY) {
+    } else if (board[i][col] !== EMPTY) {
       rowCount++;
       if (rowCount === 3) {
         winner.winner = true;
         break;
       }
     }
-
     if (board[row][i] !== EMPTY && board[row][i] !== char) {
       winner.winner = false;
     } else if (board[row][i] !== EMPTY) {
       colCount++;
-
       if (colCount === 3) {
         winner.winner = true;
         break;
@@ -28,14 +24,11 @@ function checkDiagRowAndCol(row, char, winner, board) {
     }
   }
 }
-
 export const Brain = {
   winner: {winner: false, who: ""},
-
   checkRowAndCol(char, board, row, col) {
     this.winner.who = char;
     let winCount = 0;
-
     // checking rows
     for (let i = 0; i < board.length; i++) {
       if (board[row][i] !== "." && board[row][i] !== char) {
@@ -48,10 +41,8 @@ export const Brain = {
         }
       }
     }
-
     winCount = 0;
     this.winner.dir = "col";
-
     // checking cols
     for (let i = 0; i < board.length; i++) {
       if (board[i][col] !== "." && board[i][col] !== char) {
@@ -64,19 +55,16 @@ export const Brain = {
         }
       }
     }
-
     return this.winner;
   },
-
   canCheckDiagonals(row, col) {
     return Diags.has(`${row}${col}`);
   },
-
-  diagonalWinner(char, board, row) {
+  diagonalWinner(char, board, row, col) {
+    console.log(board, row, col)
     let j = 0;
     let winCount = 0;
     this.winner.who = char;
-
     for (let i = 0; i < board.length; i++) {
       if (board[i][j++] !== char) {
         this.winner.winner = false;
@@ -85,26 +73,22 @@ export const Brain = {
         winCount++;
       }
     }
-
     if (winCount !== 3) {
       winCount = 0;
-      for (let i = board.length-1, j = 0; i >= 0; i--) {
-        if (board[i][j++] !== char) {
+      for (let i = board.length-1, j = 0; i >= 0; i--, j++) {
+        if (board[j][i] !== char) {
           this.winner.winner = false;
           break;
-        } else if (board[i][j++] !== EMPTY) {
+        } else if (board[j][i] !== EMPTY) {
           winCount++;
         }
       }
     }
-
     if (winCount !== 3) { 
-      checkDiagRowAndCol(row, char, this.winner, board);
+      checkDiagRowAndCol(row, col, char, this.winner, board);
     } else {
       this.winner.winner = true;
     }
-
     return this.winner;
   }
 };
-
